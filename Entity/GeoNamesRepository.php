5151a6b1b5492cb5cab524ab066114dd6a5d4252
Alessio <alessio.barnini@ecoseekr.com>
2013-03-14 16:28:48 +0100

    Modifiche per la nazione

--- Entity/GeoNamesRepository.php
+++ Entity/GeoNamesRepository.php
@@ -218,41 +218,8 @@ SELECT geo
         }
     }
     
-    /**
-     * tira Fuori tutte le Nazioni dal Database
-     */
-    public function cercaNazione() {
-        try {
-            $q = $this->getEntityManager()->createQuery("
-SELECT geo 
-  FROM Ephp\GeoBundle\Entity\GeoNames geo 
- WHERE geo.feature_code = 'PCLI'
- ORDER BY geo.name ASC
-               ");
-            $nazioni = $q->execute();
-            return $nazioni;
-        } catch (\Exception $e) {
-            throw $e;
-        }
-    }
     
-    /**
-     * tira Fuori tutte le Nazioni dal Database
-     * 
-     *     $q->where('p.price =:prezzo');
-    $q->andWhere('p.name =:nome');
-    $q->setParameter('prezzo', $prezzo);
-    $q->setParameter('nome', $nome);
-     * 
-     */
-    public function selectNazioni() {
-       return $this->createQueryBuilder('g')
-               ->where('g.feature_code = :fc')
-               ->andWhere('g.country_code =:cc')
-               ->setParameter('fc', 'ADM3')
-               ->setParameter('cc', 'IT')
-               ->orderBy('g.name','ASC');
-    }
+    
     
     /*
      * Restituisce il comune
